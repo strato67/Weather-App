ad3ac31f0c96954bb0d5a1110715c1fc55d0f56d
Saaransh Sharma <saaranshsharma28@gmail.com>
2021-05-11 20:25:44 -0400

    fetch

--- api.js
+++ api.js
@@ -1,11 +1,16 @@
 
 
-function helloWorld(){
 
+
+    var search = "London"
     
-    const key = "4422107a7da043cab5f80f1f757f053c";
-    const input = "Ajax";
-    const response =  fetch('https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}');
-    const data = await.response.json();
-    console.log(data)
-}
+    
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+search+'&appid=4422107a7da043cab5f80f1f757f053c')
+      
+    .then(response=>response.json())
+    .then((out) => {console.log(out);
+    })
+    .catch(err =>{throw err});
+
+
+
