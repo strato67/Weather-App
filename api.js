264ae4d3616a0610063a07c41af3b45f4bd07420
Saaransh Sharma <saaranshsharma28@gmail.com>
2021-05-12 14:30:55 -0400

    w

--- api.js
+++ api.js
@@ -1,16 +1,25 @@
-
-
-
-
-    var search = "London"
+var search;
+function searching(){
+    
     
     
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+search+'&appid=4422107a7da043cab5f80f1f757f053c')
       
     .then(response=>response.json())
-    .then((out) => {console.log(out);
+    .then((out) => {
+        var output = JSON.stringify(out)
+        document.getElementById("weather").innerHTML = output;
+        console.log(out);
     })
     .catch(err =>{throw err});
+
+    
+}
+function receiver(){
+    search = document.getElementById("city-name").value;
+    searching();
+}
+    
 
 
 
